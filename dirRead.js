08cd9d7e778745f3ccd667b7694910fd2f1384e3
dragin <alexxx55509@mail.ru>
2018-09-17 09:47:49 +0300

    fix function  getFile

--- dirRead.js
+++ dirRead.js
@@ -2,13 +2,23 @@ const path_module = require('path');
 const readdirSync = require('readdirsync2');
 
 function getDir(path) {
-    return readdirSync(path, {only: 'directory', ignoreName: ["node_modules"], recursive: false});
+	return readdirSync(path, {only: 'directory', ignoreName: ["node_modules"], recursive: false});
 }
 
 function getFile(path) {
-    return readdirSync(path, {
-        only: 'file'
-    });
+	let files = readdirSync(path, {
+		only: 'file',
+		recursive: false
+	});
+	let objFile = {};
+	files.forEach((file) => {
+		let index = file.replace(/\(\d+\).(jpg|png|jpeg|gif)|\.(jpg|png|jpeg|gif)/g, '');
+		if (!Array.isArray(objFile[index])){
+			objFile[index] = [];
+		}
+		objFile[index].push(file);
+	});
+	return objFile;
 }
 /**
  * Get struct dir  for chat bot
@@ -16,17 +26,17 @@ function getFile(path) {
  * @returns {Object}
  */
 function structFile(path) {
-    let res = {};
-    let dirs = getDir(path);
-    dirs.forEach(dir => {
-        let pathJoin = path_module.join(path, dir);
-        res[dir] = structFile(pathJoin);
-        if (Object.keys(res[dir]).length == 0){
-            res[dir] = getFile(pathJoin);
-        }
-    });
-    return res;
+	let res = {};
+	let dirs = getDir(path);
+	dirs.forEach(dir => {
+		let pathJoin = path_module.join(path, dir);
+		res[dir] = structFile(pathJoin);
+		if (Object.keys(res[dir]).length == 0){
+			res[dir] = getFile(pathJoin);
+		}
+	});
+	return res;
 }
-module.exports.structFile = structFile;
-// let url = "./test/"
-// console.log(structFile(url));
+//module.exports.structFile = structFile;
+let url = "./test/"
+console.log(structFile(url)[1]['автор1']['учебник1']);
